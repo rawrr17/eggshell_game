0c7df378658f1565f7cb30376b43ead7e96ae0ed
Rini Vaishnavi R S <rinivaishnavi2007@gmail.com>
2025-03-31 00:18:46 +0530

    My first game project commit

---
+++ Scripts/Zombie.gd
@@ -0,0 +1,25 @@
+extends CharacterBody3D
+
+var player = null
+
+const SPEED = 4.0
+
+
+@export var player_path : NodePath
+@onready var nav_agent = $NavigationAgent3D
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	player = get_node(player_path)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	velocity = Vector3.ZERO
+	
+	# Navigation
+	nav_agent.set_target_position(player.global_transform.origin)
+	var next_nav_path = nav_agent.get_next_path_position()
+	velocity = (next_nav_path - global_transform.origin).normalized() * SPEED
+	
+	look_at(Vector3(player.global_position.x, global_position.y, player.global_position.z), Vector3.UP)
+	move_and_slide()
